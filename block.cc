5d1edbdc4b99fc90679ed0fcd2ffb7817b0bd3fe
Billy <j565lee@uwaterloo.ca>
2016-11-30 23:16:23 -0500

    wee

--- block.cc
+++ block.cc
@@ -28,7 +28,7 @@ Block_I::Block_I( shared_ptr<Board> b, int s, int time ):
 
 Block_I::~Block_I() { }
 
-void Block_J::moveRight(){
+void Block_I::moveRight(){
 	if (form == form1) {
 		if (board.isEmpty(x+1, y, x+2, y, x+2, y+1, x+2, y+2) && isValid(x+2, y+2)) {
 			board->setBlock(x+2, y, board.getCurrent());
@@ -48,7 +48,7 @@ void Block_J::moveRight(){
 	}
 }
 
-void Block_J::moveLeft(){
+void Block_I::moveLeft(){
 	if (form == form1) {
 		if (board.isEmpty(x-1, y, x, y, x, y+1, x, y+2) && isValid(x-1, y)) {
 			board->setBlock(x-1, y, board.getCurrent());
@@ -68,7 +68,7 @@ void Block_J::moveLeft(){
 	}
 }
 
-void Block_J::moveDown(){
+void Block_I::moveDown(){
 	if (form == form1) {
 		if (board.isEmpty(x, y-1, x+1, y-1, x+1, y, x+1, y+1) && isValid(x, y-1)) {
 			board->setBlock(x, y-1, board.getCurrent());
