24a544c9a3d3368bff302b2317deaebbc25e0a76
hyun-gwon <hyun-gwon94@hotmail.com>
2016-12-04 11:47:29 -0500

    info class

---
+++ info.h
@@ -0,0 +1,11 @@
+#ifndef _INFO_H_
+#define _INFO_H_
+
+struct Info {
+	int r1,c1, r2,c2, r3,c3, r4,c4;
+	Info(int m1, int n1, int m2, int n2, int m3, int n3, int m4, int n4);
+	~Info();
+}
+
+
+#endif
